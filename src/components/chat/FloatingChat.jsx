c4c2dd58e242640e421040d22ce960fef3328fad
jmvillanueva-dev <jhonny.villanueva@epn.edu.ec>
2025-08-12 02:16:25 -0500

    feat: update BackendUrl in FloatingChat

--- src/components/chat/FloatingChat.jsx
+++ src/components/chat/FloatingChat.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { io } from "socket.io-client";
 
-const socket = io(import.meta.env.VITE_API_URL, {
+const socket = io(import.meta.env.VITE_BACKEND_URL, {
   transports: ["websocket"],
 });
 
